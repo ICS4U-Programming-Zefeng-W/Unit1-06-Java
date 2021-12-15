f5f8cbe70b11a6db4c6c71ab5b1c39606630d84f
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-15 05:50:05 +0000

    Added erroneous input functionality

--- Statistics.java
+++ Statistics.java
@@ -1,5 +1,6 @@
+
 /*
- * This program reads the three files and calculates and displays the mean, median,
+ * This program reads the three files, then calculates and displays the mean, median,
  * and mode of the list of integers within those files.
  *
  * By Zefeng Wang
@@ -7,62 +8,63 @@
  * */
 
 // import modules
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 // class Statistics
 class Statistics { 
-  public static void main(String[] args) throws IOException {
-    // Reads all of the files within the folder
-    File dir = new File("/home/ubuntu/environment/files");
-    File[] files = dir.listFiles();
-    List<Integer> list = new ArrayList<Integer>();
-    for (File f : files) {
-      if (f.isFile()) {
-        BufferedReader inputStream = null;
-        try {
-          // Reads each line of each file and appends it to the ArrayList
-          inputStream = new BufferedReader(new FileReader(f));
-          String line;
-          while ((line = inputStream.readLine()) != null) {
-            list.add(Integer.parseInt(line));
-          }
-        } finally {
-          if (inputStream != null) {
-            inputStream.close();
-          }
-        }
+  public static void main(String[] args) {
+    List<String> lines = Collections.emptyList();
+    try {
+
+      // Reads each line of the file and stores it into an array
+      lines = Files.readAllLines(Paths.get("/home/ubuntu/environment/files/Unit1-06/set2.txt"),
+                                 StandardCharsets.UTF_8);
+      Iterator<String> itr = lines.iterator();
+      List<Integer> list = new ArrayList<Integer>();
+      for (int i = 0; itr.hasNext(); i++) {
+        list.add(Integer.parseInt(itr.next()));
       }
+      Integer[] array = Arrays.copyOf(list.toArray(), list.size(), Integer[].class);
+      
+      // Calls the functions and outputs the result
+      double mean = calcMean(array);
+      System.out.println("The mean is " + mean);
+      double median = calcMedian(array);
+      System.out.println("The median is " + median);
+      int mode = calcMode(array);
+      System.out.println("The mode is " + mode);
+
+    } catch (Exception e) {
+      System.out.println("This file contains an invalid value.");
     }
-    // converts the ArrayList to an array containing Integer objects
-    Integer[] array = Arrays.copyOf(list.toArray(), list.size(), Integer[].class);
-    // Calls the functions and outputs the result
-    double mean = calcMean(array);
-    System.out.println("The mean is " + mean);
-    double median = calcMedian(array);
-    System.out.println("The median is " + median);
-    int mode = calcMode(array);
-    System.out.println("The mode is " + mode);
   }
+
+   
 
   // Calculate the mean of all values in the array
   private static double calcMean(Integer[] arr) {
-    double sum = 0;
+    double sum = 0.0;
     for (Integer num : arr) {
       sum += num;
     }
     return sum / arr.length;
   }
 
-  // Sorts the array and calculate the median of all values in the array
+  // Calculate the median of all values in the array
   private static double calcMedian(Integer[] arr) {
+
+    // Gets the index at the middle of the array and sorts the array
+    double middleIndex = (arr.length - 1) / 2.0;
     Arrays.sort(arr);
-    double middleIndex = (arr.length-1) / 2;
+
+    // Check to see if the index is an integer
     if (middleIndex % 1 == 0) {
       return arr[(int) middleIndex];
     } else {
@@ -91,3 +93,5 @@ class Statistics {
     return maxValue;
   }  
 }
+
+
